3c3d357db269eac1e6b74d2a88b236a3eb171807
codingfanatic <lclarkerichard@yahoo.com>
2020-10-07 21:45:50 -0400

    Pulled addresses into the List Addresses Activity -Instantiated Arrylist object -Instantiated ArrayAdapter using Activity, list_listview xml layout, and ArrayList object -Assigned reference to ListView object using List View layout from activity_main xml layout -Set Adapter for the ListView object -Assigned a reference to the DatabaseReference object -Add a Child Event Listener to the DatabaseReference object -Override the onChildAdded method for the ChildEventListener for the DatabaseReference object

--- app/src/main/java/com/codingfanatic/travelparttwo/List_Addresses.java
+++ app/src/main/java/com/codingfanatic/travelparttwo/List_Addresses.java
@@ -10,36 +10,80 @@ List Addresses
 7. Set the ArrayAdapter for the ListView
 ************************************************************************************************************************************************************
 */
+
+//6. Override the onChildAdded method for the ChildEventListener for the DatabaseReference object
 package com.codingfanatic.travelparttwo;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
 public class List_Addresses extends AppCompatActivity {
     
-    //4. Create a String array containing each of the the addresses
-    String[] addresses = {"123\nMain Street St. Louisville\nOH 4307",
-                            "432\nMain Long Road St. Louisville\nOH 43071",
-                            "786\nHigh Street Pollocksville\nOH 43071"};
-    
-    //5. Create ListView and ArrayAdapter objects
+    //1. Instantiated ListView and ArrayList objects
     ListView listview;
-    ArrayAdapter adapter;
-
+    ArrayList<String> mArrayList = new ArrayList<>();
+    DatabaseReference myRef;
+    
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list);
         getSupportActionBar().setTitle("View List");
 
-        //6. Instantiate the ListView and ArrayAdapter objects
-        listview = findViewById(R.id.address_list);
-        adapter = new ArrayAdapter<String>(this, R.layout.list_listview, addresses);
+        //2. Instantiated ArrayAdapter using Activity, list_listview xml layout, and ArrayList object
+        //3. Assigned reference to ListView object using List View layout from activity_main xml layout
+        //   Set Adapter for the ListView object
+        listview = (ListView) findViewById(R.id.address_list);
 
-        //7. Set the ArrayAdapter for the ListView    
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_listview, mArrayList);
         listview.setAdapter(adapter);
+        
+        //4. Assigned a reference to the DatabaseReference object
+        myRef = FirebaseDatabase.getInstance().getReference();
+
+        //5. Add a Child Event Listener to the DatabaseReference object
+        myRef.addChildEventListener(new ChildEventListener() {
+
+            //6. Override the onChildAdded method for the ChildEventListener for the DatabaseReference object
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                String value = snapshot.getValue(String.class);
+                mArrayList.add(value);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
