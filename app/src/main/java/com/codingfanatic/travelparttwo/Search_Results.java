af27b116a1fda3ad91547460860ca8cea151f2b0
codingfanatic <lclarkerichard@yahoo.com>
2020-10-14 17:35:54 -0400

    Refactored code The app now pulls addresses from the back end into the app in order to instantiate the Home objects

--- app/src/main/java/com/codingfanatic/travelparttwo/Search_Results.java
+++ app/src/main/java/com/codingfanatic/travelparttwo/Search_Results.java
@@ -1,6 +1,8 @@
 package com.codingfanatic.travelparttwo;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.KeyEvent;
@@ -10,9 +12,20 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
 public class Search_Results extends AppCompatActivity {
+    
     ListView results_list;
+    ArrayList<String> mArrayList = new ArrayList<>();
     EditText editText;
+    DatabaseReference myRef;
 
     ArrayAdapter original_adapter, display_adapter, no_results_adapter;
 
@@ -22,33 +35,63 @@ public class Search_Results extends AppCompatActivity {
         setContentView(R.layout.search);
         getSupportActionBar().setTitle("Search Zipcode");
 
-        /*TODO
-        Add Addresses from Home.class to RealtimeDatabase
-        */
-        Home first = new Home(Home.ADDRESS_ONE);
-        Home second = new Home(Home.ADDRESS_TWO);
-        Home third = new Home(Home.ADDRESS_THREE);
-
         results_list = findViewById(R.id.result_list);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_listview, mArrayList);
+        results_list.setAdapter(adapter);
+
+        myRef = FirebaseDatabase.getInstance().getReference();
+
         display_adapter = new ArrayAdapter<Home>(this, R.layout.list_listview);
         original_adapter = new ArrayAdapter<Home>(this, R.layout.list_listview);
         no_results_adapter = new ArrayAdapter<String>(this, R.layout.list_listview);
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                String value = snapshot.getValue(String.class);
+                Home homeObject = new Home(value);
+                original_adapter.add(homeObject);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        /*
+        Home first = new Home(Home.ADDRESS_ONE);
+        Home second = new Home(Home.ADDRESS_TWO);
+        Home third = new Home(Home.ADDRESS_THREE);
         
-        /*TODO
-        Override the onChildAdded method for the ChildEventListener for the DatabaseReference object
-        Use the String imported from the Realtime Database to instantiate a Home obkect
-        original_adapter.add(whateverThisObjectWillBeCalled)
-        */
         original_adapter.add(first);
         original_adapter.add(second);
         original_adapter.add(third);
+        */
+
         no_results_adapter.add("NO RESULTS FOUND");
 
             for(int i = 0; i < original_adapter.getCount(); i++){
                 display_adapter.add(original_adapter.getItem(i));
             }
 
-        results_list.setAdapter(display_adapter);
+        results_list.setAdapter(original_adapter);
 
         //OnEditorActionListener for the keyboard. This logic defines how the list updates
         editText = findViewById(R.id.input);
